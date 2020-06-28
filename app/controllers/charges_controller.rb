46a7e51278664b97b61af3be6d8958669fe2bcd6
Mikey Ettlinger <mikeyettlinger@gmail.com>
2020-06-28 22:41:04 +0100

    dont commit

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -22,4 +22,11 @@ class ChargesController < ApplicationController
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
+  private
+
+  def charges_params
+    params.permit(:stripeEmail, :stripeToken, :order_id)
+  end
+
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,7 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => ENV['pk_test_xF61NzZKqAY359NF0YuBtp59000aPPR7ZH'],
+  :secret_key      => ENV['sk_test_z4omCsuISpY1Tq9oz6GFcCVa00laIotMB7']
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
